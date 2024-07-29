9effcde092844b573ac72f327405bb15cb75d0b1
QuattrosCafeyHelados <klowsend26@gmail.com>
2024-07-29 23:38:52 +0200

    Add files via upload

--- scriptsmenu.js
+++ scriptsmenu.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const productos = [
-        { nombre: "Agua", descripcion: "Con o sin gas",imagen: "img/agua.jfif" },
-        { nombre: "Gaseosa", descripcion: "Coca-cola, Fanta, 7up", imagen: "img/gaseosa.jfif" },
-        { nombre: "Exprimido", descripcion: "De naranja fresca", imagen: "img/exprimido.jfif" },
-        { nombre: "Licuado de fruta", descripcion: "Con fruta fresca de estacion", imagen: "img/licuado.jfif" },
+        { nombre: "Agua $500", descripcion: "Con o sin gas",imagen: "img/agua.jfif" },
+        { nombre: "Gaseosa", descripcion: "Linea Coca-Cola",imagen: "img/agua.jfif" },
+        { nombre: "Agua saborizada", descripcion: "Aquarius", imagen: "img/gaseosa.jfif" },
+        { nombre: "Exprimido", descripcion: "De naranja", imagen: "img/exprimido.jfif" },
+        { nombre: "Licuados", descripcion: "Fruta fresca con Leche o Agua", imagen: "img/licuado.jfif" },
         { nombre: "Batido de helado", descripcion: "Leche + Tu helado preferido", imagen: "img/batido.jfif" }
     ];
 
@@ -41,22 +42,21 @@ Container = document.getElementById("productos");
 
 document.addEventListener("DOMContentLoaded", function() {
     const productos2 = [
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" }
+        { nombre: "Expresso", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Americano", descripcion: "", imagen: "img/batido.jpg" },
+        { nombre: "Cortado", descripcion: "", imagen: "img/cafe.jpg" },
+        { nombre: "Cortado doble", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Café con leche", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Lagrima", descripcion: "", imagen: "img/batido.jpg" },
+        { nombre: "Lagrima doble", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Capuccino", descripcion: "Café, leche, chocolate, crema y canela", imagen: "img/cafe.jpg" },
+        { nombre: "Submarino", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Affogato", descripcion: "Café con una bocha de helado",imagen: "img/cafe.jpg" },
+        { nombre: "Café frappe", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
+        { nombre: "Té", descripcion: "Con o sin leche", imagen: "img/alfajor.jpg" },
+        { nombre: "Vaso de leche", descripcion: "Caliente o fria", imagen: "img/cafe.jpg" },
+        { nombre: "Leche de almendras", descripcion: "Adicional", imagen: "img/alfajor.jpg" },
+        { nombre: "Crema", descripcion: "Adicional", imagen: "img/alfajor.jpg" }
     ];
 
     const productos2Container = document.getElementById("productos2");
@@ -93,18 +93,18 @@ Container = document.getElementById("productos");
 
 document.addEventListener("DOMContentLoaded", function() {
     const productos3 = [
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" }
+        { nombre: "Medialuna", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Alfajor", descripcion: "Consultar sabores", imagen: "img/batido.jpg" },
+        { nombre: "Alfajor de maicena", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Cookies", descripcion: "Consultar sabores", imagen: "img/cafe.jpg" },
+        { nombre: "Borrachitos", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Muffins", descripcion: "Consultar sabores",imagen: "img/cafe.jpg" },
+        { nombre: "Porcion de budin", descripcion: "Consultar sabores", imagen: "img/batido.jpg" },
+        { nombre: "Brownie", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Pastafrola", descripcion: "", imagen: "img/cafe.jpg" },
+        { nombre: "Tarta de ricota", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Tarta de coco", descripcion: "", imagen: "img/batido.jpg" },
+        { nombre: "Crumble de manzana", descripcion: "", imagen: "img/alfajor.jpg" }
         
     ];
 
@@ -143,10 +143,10 @@ Container = document.getElementById("productos");
 
 document.addEventListener("DOMContentLoaded", function() {
     const productos4 = [
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
+        { nombre: "Medialuna", descripcion: "Jamon & queso",imagen: "img/cafe.jpg" },
+        { nombre: "Tostado de miga", descripcion: "Jamon & queso", imagen: "img/batido.jpg" },
+        { nombre: "Porcion de tostadas", descripcion: "2 dips a eleccion: queso-crema/mermelada/ddl", imagen: "img/alfajor.jpg" },
+        { nombre: "Sanguche de focaccia", descripcion: "Jamon cocido/crudo o Mortadela & queso", imagen: "img/cafe.jpg" },
         
     ];
 
@@ -185,28 +185,27 @@ Container = document.getElementById("productos");
 
 document.addEventListener("DOMContentLoaded", function() {
     const productos5 = [
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Café Americano", descripcion: "Café negro clásico",imagen: "img/cafe.jpg" },
-        { nombre: "Latte", descripcion: "Café con leche espumada", imagen: "img/batido.jpg" },
-        { nombre: "Capuchino", descripcion: "Café con leche y espuma", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
-        { nombre: "Té Verde", descripcion: "Té saludable y refrescante", imagen: "img/cafe.jpg" },
-        { nombre: "Pastel de Chocolate", descripcion: "Delicioso pastel de chocolate", imagen: "img/alfajor.jpg" },
+        { nombre: "Americana", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Vainilla", descripcion: "", imagen: "img/batido.jpg" },
+        { nombre: "Chocolate", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Chocolate %70", descripcion: "", imagen: "img/cafe.jpg" },
+        { nombre: "Super DDL", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "DDL Granizado", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Frutilla a la crema", descripcion: "", imagen: "img/batido.jpg" },
+        { nombre: "Menta granizada", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Mascarpone", descripcion: "", imagen: "img/cafe.jpg" },
+        { nombre: "Alfajor", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Tramontana", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Mantecol", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Lemon Pie", descripcion: "", imagen: "img/cafe.jpg" },
+        { nombre: "Tiramisú", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Crema cookie", descripcion: "",imagen: "img/cafe.jpg" },
+        { nombre: "Flan", descripcion: "", imagen: "img/batido.jpg" },
+        { nombre: "Banana Split", descripcion: "", imagen: "img/alfajor.jpg" },
+        { nombre: "Balcarce", descripcion: "", imagen: "img/cafe.jpg" },
+        { nombre: "Limón", descripcion: "Al agua", imagen: "img/alfajor.jpg" },
+        { nombre: "Mandarina", descripcion: "Al agua", imagen: "img/cafe.jpg" },
+        { nombre: "Frutilla", descripcion: "Al agua", imagen: "img/alfajor.jpg" },
 
     ];
 
